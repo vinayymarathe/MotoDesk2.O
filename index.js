c3f8f83fd17a4348517b08d6ed3594680e16693a
nakshatra3 <116239546+nakshatra3@users.noreply.github.com>
2024-10-16 17:48:02 +0530

    changes

--- index.js
+++ index.js
@@ -4,64 +4,9 @@ const path = require("path");
 const dealer = require("./config/dealer.config");
 const Inventory = require("./config/Inventory.config");
 const InventoryRoute = require("./routes/inventory.route");
-const session = require("express-session");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 const PORT = 3000;
-
-app.use(
-  session({
-    secret: "123456789",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
-
-// Google OAuth 2.0 strategy
-passport.use(new GoogleStrategy({
-    clientID: '499154116072-6fnr6n35bkuap4ilathhq6gcdsimouf3.apps.googleusercontent.com',
-    clientSecret: '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',
-    callbackURL: 'http://localhost:3000/auth/google/callback'
-},
-async (accessToken, refreshToken, profile, done) => {
-    try {
-        // Check if user already exists
-        const existingUser = await dealer.findOne({ email: profile.emails[0].value });
-        
-        if (existingUser) {
-            // If user exists, return the user
-            return done(null, existingUser);
-        }
-
-        // If user doesn't exist, create a new one
-        const newUser = {
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            phone: '', // Assign default or prompt the user to provide this later
-            location: '', // Assign default or prompt the user to provide this later
-            username: profile.id, // Use Google ID or generate a username
-            password: '' // No password for Google login
-        };
-
-        const createdUser = await dealer.create(newUser);
-        return done(null, createdUser);
-    } catch (error) {
-        return done(error);
-    }
-}));
 
 
 app.set("view engine", "ejs");
@@ -85,16 +30,6 @@ app.get("/invent", (req, res) => {
   res.render("addInventory");
 });
 
-// Handle Google authentication
-app.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-app.get("/auth/google/callback", passport.authenticate("google", { 
-    failureRedirect: "/login" 
-  }), (req, res) => {
-    // Successful authentication, redirect to the dashboard or home page
-    res.render("dashboard", { user: req.user });
-  }
-);
 
 // Register route
 app.post("/register", async (req, res) => {
